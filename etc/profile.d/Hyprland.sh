98b99a729d24d0cbaacb45f22b737b089b30f7b4
Vasiliy Stelmachenok <ventureo@yandex.ru>
2023-03-06 10:02:54 +0300

    Add check for nouveau

--- etc/profile.d/Hyprland.sh
+++ etc/profile.d/Hyprland.sh
@@ -1,4 +1,4 @@
-if [ -d /sys/module/nvidia ] && [ ! -d /sys/module/i915 ] && [ ! -d /sys/module/amdgpu ]; then
+if [ -d /sys/module/nvidia ] && [ ! -d /sys/module/i915 ] && [ ! -d /sys/module/amdgpu ] && [ ! -d /sys/module/nouveau ]; then
     export WLR_NO_HARDWARE_CURSORS=1
     export GBM_BACKEND=nvidia-drm
     export EGL_PLATFORM=wayland
